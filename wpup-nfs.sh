a85af179461daac638ce0fbf7419a71e00092029
Sumon Paul <43354366+SumonPaul18@users.noreply.github.com>
2024-07-05 21:50:08 +0600

    Update wpup-nfs.sh

--- wpup-nfs.sh
+++ wpup-nfs.sh
@@ -1,44 +1,55 @@
 #!/bin/bash
-mkdir -p /nfs-share
-mount 192.168.0.96:/nfs-share /nfs-share
-ls -l /nfs-share
-mkdir -p /nfs-share/wordpress
-mkdir -p /nfs-share/wordpress/wpdata
-mkdir -p /nfs-share/wordpress/mysql
-chmod 775 -R /nfs-share/wordpress
-cd /nfs-share/wordpress
+bgreen='\033[1;32m'
+red='\033[0;31m'
+nc='\033[0m'
+bold="\033[1m"
+blink="\033[5m"
+echo -e "${bgreen}Create a Directory For Store WordPess ${nc} "
+echo
+echo
+ll /nfs-share
+read -p "$(echo -e "${bgreen}${bold}${blink}Type a Directory Name for store Wordpess: ${nc}")" WPDIR
+echo
+echo
+read -p "$(echo -e "${bgreen}${bold}${blink}Type Unused Port For Browse WordPess: ${nc}")" WPPORT
+mkdir -p /nfs-share/docker/$WPDIR
+mkdir -p /nfs-share/docker/$WPDIR
+mkdir -p /nfs-share/docker/$WPDIR/wpdata
+mkdir -p /nfs-share/docker/$WPDIR/mysql
+chmod 775 -R /nfs-share/docker/$WPDIR
+cd /nfs-share/docker/$WPDIR
 ls
-cat <<EOF | sudo tee /nfs-share/wordpress/docker-compose.yml
+cat <<EOF | sudo tee /nfs-share/docker/$WPDIR/docker-compose.yml
 version: "3.8" 
 services:
   db:
     image: mysql:latest
     restart: always
     volumes:
-      - /nfs-share/wordpress/mysql/:/var/lib/mysql
+      - /nfs-share/docker/$WPDIR/mysql/:/var/lib/mysql:rw
     environment:
       MYSQL_ROOT_PASSWORD: centos@123
       MYSQL_DATABASE: wordpress
       MYSQL_USER: sysadmin
       MYSQL_PASSWORD: centos@123
-  wordpress:
+  $WPDIR:
     depends_on:
       - db
-    image: wordpress:latest
+    image: $WPDIR:latest
     restart: always
     ports:
-      - "8000:80"
+      - "$WPPORT:80"
     environment:
       WORDPRESS_DB_HOST: db:3306
       WORDPRESS_DB_USER: sysadmin
       WORDPRESS_DB_PASSWORD: centos@123
       WORDPRESS_DB_NAME: wordpress
     volumes:
-      - /nfs-share/wordpress/wpdata/:/var/www/html:rw
+      - /nfs-share/docker/$WPDIR/wpdata/:/var/www/html:rw
            
 EOF
 
-cd /nfs-share/wordpress
+cd /nfs-share/docker/$WPDIR
 ls
 docker compose up -d
 docker compose ps
